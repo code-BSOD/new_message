3a45b07e4cb51834ae573c09a5ab02ea92f070ff
code-BSOD <mishkat2008@gmail.com>
2023-11-26 18:42:24 +0100

    back to waitress

--- app.py
+++ app.py
@@ -54,6 +54,6 @@ def thank_you():
 
 
 if __name__ == '__main__':
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=8080)
-    app.run(host="0.0.0.0", port=8080)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    # app.run(host="0.0.0.0", port=8080)
